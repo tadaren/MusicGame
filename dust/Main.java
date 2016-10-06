9fb53ed14893f9c78427b78237b6ca62f5dabb13
tadaren <ptymaato@yahoo.co.jp>
2016-10-06 16:47:46 +0900

    Add files via upload

--- dust/Main.java
+++ dust/Main.java
@@ -1,34 +1,22 @@
 package dust;
 
-import java.awt.GridLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Main extends JFrame{
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main();
 	}
 	
 	public Main(){
-		setBounds(10, 10, 300, 300);
+		setBounds(10, 10, 500, 800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
 		
-		JPanel mainPanel = new JPanel();
+		MainPanel mainPanel = new MainPanel();
 		add(mainPanel);
-		mainPanel.setLayout(new GridLayout(1, 4));
-		
-		SubPanel panel1 = new SubPanel('d');
-		SubPanel panel2 = new SubPanel('f');
-		SubPanel panel3 = new SubPanel('j');
-		SubPanel panel4 = new SubPanel('k');
-		mainPanel.add(panel1);
-		mainPanel.add(panel2);
-		mainPanel.add(panel3);
-		mainPanel.add(panel4);
 		
 		setVisible(true);
 		
