aeb28958b835e64a0a7e03936281112c85142fa9
tadaren <ptymaato@yahoo.co.jp>
2016-10-27 08:44:59 +0900

    Update Debug.java

--- src/dust/Debug.java
+++ src/dust/Debug.java
@@ -4,11 +4,7 @@ public class Debug {
 
 	public static void main(String[] args) {
 //		new GamePanel();
-<<<<<<< HEAD
 		new GamePanel("./music/music/note.txt");
-=======
-		new GamePanel("./music/music1/music_note.txt");
->>>>>>> origin/master
 	}
 
 }
